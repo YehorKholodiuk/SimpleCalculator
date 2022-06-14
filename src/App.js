12b01789e6e2242980257aae90fad4a927a3529a
ASUS <Yehor Kholodiuk>
2022-06-14 15:44:27 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [number1, setNumber1] = useState(10)
+    const [number2, setNumber2] = useState(10)
+    const changeNumber1 = (event) => {
+        setNumber1(+event.target.value);
+    }
+    const changeNumber2 = (event) => {
+        setNumber2(+event.target.value);
+    }
+    return (
+        <div className="App">
+            <h1>{number1}</h1>
+            <input value={number1} onChange={changeNumber1}/>
+
+            <input value={number2} onChange={changeNumber2}/>
+            <h1>{number1}+{number2}={number1 + number2}</h1>
+        </div>
+    );
 }
 
 export default App;
